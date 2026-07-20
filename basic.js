06f432957f55efccf63a02fa6f05105d7f64227e
Aravind112703 <aravindmathi93@gmail.com>
2026-07-20 21:42:50 +0530

    js basics

---
+++ basic.js
@@ -0,0 +1,18 @@
+// print the values
+
+console.log("playwright is a popular automation tool");
+
+// single line comment
+
+//console.log("Cypress");
+
+
+/*
+
+
+
+abcdefghifgtkjnjkn
+console.log()
+
+
+*/
---
+++ variables.js
@@ -0,0 +1,43 @@
+
+
+//variables
+
+
+
+
+// var - old way of declaring variables
+// let 
+// const
+
+
+// var automation = "selenium" // no use
+
+// let automation = "cypress" // 2nd
+
+// const automation = "playwright" // 1st
+
+
+
+
+// let automation = "playwright"
+
+//  automation = "cypress"
+
+
+// console.log(automation)
+
+
+// const automation = "playwright"
+
+// // automation = "cypress"
+
+// console.log(automation);
+
+
+
+const automation = "playwright"
+
+const Automation = "cypress"
+
+console.log(automation);
+
