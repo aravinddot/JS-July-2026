0a52b401dfc1b3d0214e82b79c7e66229fcbac69
Aravind112703 <aravindmathi93@gmail.com>
2026-08-05 20:10:12 +0530

    js completed

--- expceptionHandling.js
+++ expceptionHandling.js
@@ -10,6 +10,8 @@ try {
    console.log(err.message)
 //    console.log(err.name)
 //    console.log(err.stack)
+
+// throw new Error("this is a custom error")
 } finally {
     console.log("this block will always execute")
 }
